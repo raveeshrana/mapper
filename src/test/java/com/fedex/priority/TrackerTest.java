c176eb27e8abd906fd5a5c737136ef0c894e6497
Raveesh Rana <raveesh.rana@hilton.com>
2020-05-22 13:32:50 -0500

    Generalizing functions

--- src/test/java/com/fedex/priority/TrackerTest.java
+++ src/test/java/com/fedex/priority/TrackerTest.java
@@ -17,11 +17,13 @@ public class TrackerTest {
     AccountCache.put("12345", "S", "01,02,03");
     AccountCache.put("12346", "R", "01");
     AccountCache.put("12348", "T", "01,04");
+    AccountCache.put("12348", "S", "01,04");
+    AccountCache.put("12348", "R", "01,04");
     AccountCache.put("12345", "T", "04");
   }
 
   @Test()
-  public void test(){
+  public void publishAll3(){
     final AccountKey[] detailsList = MapAccount
       .getPublishAccountList("12345", "12346", "12348", "01");
     assertEquals(3, detailsList.length);
@@ -33,5 +35,53 @@ public class TrackerTest {
 
     assertEquals("12348", detailsList[2].getAccountNumber());
     assertEquals("T", detailsList[2].getAccountType());
+  }
+
+  @Test()
+  public void publishShipperAndThirdParty(){
+    final AccountKey[] detailsList = MapAccount
+      .getPublishAccountList("12345", "1234", "12348", "01");
+    assertEquals(2, detailsList.length);
+    assertEquals("12345", detailsList[0].getAccountNumber());
+    assertEquals("S", detailsList[0].getAccountType());
+
+    assertEquals("12348", detailsList[1].getAccountNumber());
+    assertEquals("T", detailsList[1].getAccountType());
+  }
+
+  @Test()
+  public void publishRecipientAndThirdParty(){
+    final AccountKey[] detailsList = MapAccount
+      .getPublishAccountList("1234", "12346", "12348", "01");
+    assertEquals(2, detailsList.length);
+    assertEquals("12346", detailsList[0].getAccountNumber());
+    assertEquals("R", detailsList[0].getAccountType());
+
+    assertEquals("12348", detailsList[1].getAccountNumber());
+    assertEquals("T", detailsList[1].getAccountType());
+  }
+
+  @Test()
+  public void publishShipperAndRecipientParty(){
+    final AccountKey[] detailsList = MapAccount
+      .getPublishAccountList("12348", "12346", "12348", "01");
+    assertEquals(2, detailsList.length);
+    assertEquals("12348", detailsList[0].getAccountNumber());
+    assertEquals("S", detailsList[0].getAccountType());
+
+    assertEquals("12346", detailsList[1].getAccountNumber());
+    assertEquals("R", detailsList[1].getAccountType());
+  }
+
+  @Test()
+  public void publishRecipientAndShipperParty(){
+    final AccountKey[] detailsList = MapAccount
+      .getPublishAccountList("12345", "12348", "12348", "01");
+    assertEquals(2, detailsList.length);
+    assertEquals("12345", detailsList[0].getAccountNumber());
+    assertEquals("S", detailsList[0].getAccountType());
+
+    assertEquals("12348", detailsList[1].getAccountNumber());
+    assertEquals("R", detailsList[1].getAccountType());
   }
 }
