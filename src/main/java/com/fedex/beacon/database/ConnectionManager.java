327dc708e55c63b283d8d4e02fe2e68e46c50da4
Raveesh Rana <raveesh.rana@hilton.com>
2020-05-26 14:05:50 -0500

    Adding variable constructor

--- src/main/java/com/fedex/beacon/database/ConnectionManager.java
+++ src/main/java/com/fedex/beacon/database/ConnectionManager.java
@@ -9,6 +9,23 @@ public class ConnectionManager {
 
   private static HikariDataSource ds;
 
+  public static void init(final String databaseUrl, final String driverClass, final String userName, final String password) {
+    try {
+      HikariConfig config = new HikariConfig();
+      config.setJdbcUrl(databaseUrl);
+      config.setUsername(userName);
+      config.setPassword(password);
+      config.setConnectionTimeout(60*1000);
+      config.setDriverClassName(driverClass);
+      config.setMaximumPoolSize(128);
+      config.setMaximumPoolSize(10);
+      config.setIdleTimeout(10 * 60 * 1000);
+      config.setReadOnly(true);
+      ds = new HikariDataSource(config);
+    } catch (Exception ex) {
+      throw new RuntimeException("Not able to connect to database."+ ex.getCause());
+    }
+  }
   public static void init(final ConnectionInfo info) {
     try {
       HikariConfig config = new HikariConfig();
